15fc90da1b53958ee761fbbad484bb0af1282881
HipolitoRosado <paulserrano58@gmail.com>
2020-03-01 09:26:31 -0600

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        public enum Weekdays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {            //Create a list to hold tasdks
+            List<string> tasklist = new List<string>();
+
+            //for each value in the Enum...let the user create a task.
+            foreach (Weekdays day in Enum.GetValues(typeof(Weekdays)))
+            {
+                Console.WriteLine("Enter a new task" + day);
+                string task = day + ":" + Console.ReadLine();
+                tasklist.Add(task);
+            }
+            //print all of the task.
+            foreach (string task in tasklist)
+            {
+
+
+
+
+
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
